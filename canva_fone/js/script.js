880f4b1103fb20b2a28ce6f66986df963b3973a7
jenula <jenula2108@gmail.com>
2024-05-25 09:14:19 +0300

    a

---
+++ canva_fone/js/script.js
@@ -0,0 +1,13 @@
+"use  strict";
+// document.write("hello world");
+// let name = "jenula",
+//   age = "39";
+// document.write(`hello ${name}, my age ${age} old.`);
+// let a = parseInt(prompt("ввведите значение a"));
+// let b = parseFloat(prompt("ввведите значение b"));
+
+// let s = a + b;
+// alert(`сумма а + б = ${s.toFixed(2)}`);
+
+// console.log(`сумма а + б = ${s}`);
+// document.write(`<br> сумма а + б = ${s}`);
